e7d46c7bd840529bfd1fafb8363a93e9e0510a39
Kostya <kostikbushmelev@gmail.com>
2023-12-07 07:25:10 +0300

    add Sun system simulation

--- FlyObjBox.py
+++ FlyObjBox.py
@@ -1,61 +1,46 @@
 from math import *
 
-
 class FlyObjBox:
-    m = []
-    v_x = []
-    v_y = []
-    x = []
-    y = []
+
+    # consts
+    G = 6.674 * 10 ** -11
+    scale = 8.9 / 12 * (10 ** 8)
+
+    planets = []
     f_x = 0
     f_y = 0
-<<<<<<< Updated upstream
-    # change this constant
-    G = 1
-=======
-    G = 6.674 * 10**-11
->>>>>>> Stashed changes
 
-    def addPlanet(self, planet):
-        self.v_x.append(planet.speed_x)
-        self.v_y.append(planet.speed_y)
-        self.x.append(planet.position_x)
-        self.y.append(planet.position_y)
-        self.m.append(planet.weight)
+    def add_planet(self, planet):
+        self.planets.append(planet)
 
-    def calcAcceleration(self, i, N):
-        for j in range(0, N):
+    def set_planets(self, planets):
+        self.planets = planets
+
+    def calc_acceleration(self, i, count_planets):
+        for j in range(0, count_planets):
             if i != j:
-                r = sqrt((self.x[j] - self.x[i]) ** 2 + (self.y[j] - self.y[i]) ** 2)
-                F = self.G * self.m[i] * self.m[j] / r ** 2
-<<<<<<< Updated upstream
-                cosine = (self.x[j] - self.x[i]) / r
-                sinus = (self.y[j] - self.y[i]) / r
-=======
-                cosine = 1.392e8 / sqrt(10) * (self.x[j] - self.x[i]) / r
-                sinus = 1.392e8 / sqrt(10) * (self.y[j] - self.y[i]) / r
->>>>>>> Stashed changes
+                r = sqrt((self.planets[j].position_x - self.planets[i].position_x) ** 2 +
+                         (self.planets[j].position_y - self.planets[i].position_y) ** 2) * self.scale
+                F = self.G * self.planets[i].weight * self.planets[j].weight / r ** 2
+                cosine = self.scale * (self.planets[j].position_x - self.planets[i].position_x) / r
+                sinus = self.scale * (self.planets[j].position_y - self.planets[i].position_y) / r
                 F_x = F * cosine
                 F_y = F * sinus
                 self.f_x += F_x
                 self.f_y += F_y
-        a_x = self.f_x / self.m[i]
-        a_y = self.f_y / self.m[i]
+        a_x = self.f_x / self.planets[i].weight
+        a_y = self.f_y / self.planets[i].weight
         return a_x, a_y
 
-    def calcSpeed(self, i, a_x, a_y, dt):
-        v_x = self.v_x[i] + a_x * dt
-        v_y = self.v_y[i] + a_y * dt
+    def calc_speed(self, i, a_x, a_y, dt):
+        v_x = self.planets[i].speed_x + a_x * dt
+        v_y = self.planets[i].speed_y + a_y * dt
         return v_x, v_y
 
-    def calcPosition(self, i, v_x, v_y, dt):
-        x = self.x[i] + v_x * dt
-        y = self.y[i] + v_y * dt
+    def calc_position(self, i, v_x, v_y, dt):
+        x = self.planets[i].position_x + v_x * dt
+        y = self.planets[i].position_y + v_y * dt
         return x, y
 
     def reset(self):
-        self.m = []
-        self.v_x = []
-        self.v_y = []
-        self.x = []
-        self.y = []
+        self.planets = []
